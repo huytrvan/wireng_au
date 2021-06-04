b3a300b1713d37d9d5ade2759d5a69a6ad97ca9e
huy_trvan <huy_trvan@yahoo.com>
2021-06-04 10:29:07 +0700

    dev: test install font, success

---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Testing testing</h1>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    mode: 'jit',
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        fontFamily: {
+            'sans': 'Roboto Condensed, Arial, sans-serif',
+        },
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+    corePlugins: {
+        preflights: false,
+    }
+}
