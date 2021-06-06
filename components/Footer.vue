598b71afa662b9eb289f13890dc4ea0ebd5dc3f5
huy_trvan <huy_trvan@yahoo.com>
2021-06-06 08:28:28 +0700

    feat_homepage: feat footer currentYear

---
+++ components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer
+    class="
+      text-center
+      py-4
+      bg-gradient-to-b
+      from from-gray-200
+      to-gray-50
+      text-gray-700 text-sm
+    "
+  >
+    Copyright © 2010-{{ this.currentYear }} WirEng® Australia & New Zealand. All
+    rights reserved. Prices and specifications are subject to change without
+    notice.
+  </footer>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      currentYear: new Date().getFullYear(),
+    };
+  },
+};
+</script>
