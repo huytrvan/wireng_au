d715029dc9efe8ca3121d395811b4501ec4af07c
huy_trvan <huy_trvan@yahoo.com>
2021-06-06 09:04:14 +0700

    feat_homepage: minor margin styling layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,8 +2,8 @@
   <div class="font-sans">
     <Header class="mb-4 px-6" />
     <div class="px-8 sm:px-12">
-      <Nuxt class="mb-12" />
-      <ContactAbout class="mb-8" />
+      <Nuxt />
+      <ContactAbout class="mb-12 mt-16" />
     </div>
     <Footer />
   </div>
