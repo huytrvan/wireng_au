bd47adad143b026058047b08b9183f359aea7d63
huy_trvan <huy_trvan@yahoo.com>
2021-06-10 16:10:54 +0700

    feat: breadcrumb more reflective of the url slug

--- components/modem/BreadCrumb.vue
+++ components/modem/BreadCrumb.vue
@@ -1,22 +1,44 @@
 <template>
   <div class="text-sm">
-    <NuxtLink to="/" class="text-blue-400 hover:underline">Home</NuxtLink>
-    <span>/</span>
-    <NuxtLink :to="'/' + this.brand" class="text-blue-400 hover:underline"
-      >{{ brand }}
-    </NuxtLink>
-    <span>/</span>
-    <span>{{ model }}</span>
+    <span>
+      <NuxtLink to="/" class="text-blue-400 hover:underline"> Home </NuxtLink>
+      /
+    </span>
+    <span v-for="url in otherUrls" :key="url.id">
+      <NuxtLink
+        :to="'/' + url.toLowerCase()"
+        class="text-blue-400 hover:underline"
+        >{{ url }}
+      </NuxtLink>
+      /
+    </span>
+    <span>{{ currentUrl }}</span>
   </div>
 </template>
 <script>
 export default {
-  props: ["jsonData"],
-  data() {
-    return {
-      brand: this.jsonData[0][1],
-      model: this.jsonData[1][1],
-    };
+  props: ["slug"],
+  computed: {
+    slugItems: function () {
+      let slugItems = this.slug.split("/");
+      slugItems = slugItems.map(function (value) {
+        let capitalizedFirstLetter = value[0].toUpperCase();
+        let theRest = value.slice(1);
+        return capitalizedFirstLetter + theRest;
+      });
+      return slugItems;
+    },
+    totalItems: function () {
+      return this.slugItems.length;
+    },
+    currentUrl: function () {
+      let currentUrl = this.slugItems[this.totalItems - 1];
+      return currentUrl;
+    },
+    otherUrls: function () {
+      let otherUrls = this.slugItems.slice(0, this.totalItems - 1);
+      return otherUrls;
+    },
   },
 };
 </script>
