f68ae5ff8ba87fd0e6f378bb9c2c1719cc0ac415
huy_trvan <huy_trvan@yahoo.com>
2021-06-10 18:52:08 +0700

    fix: update data to match breaking changes in variable names and computed

--- components/modem/Gallery.vue
+++ components/modem/Gallery.vue
@@ -2,30 +2,34 @@
   <div class="">
     <agile
       ref="main"
-      @after-change="$refs.thumb.goTo($event.currentSlide)"
+      @after-change="$refs.thumbnail.goTo($event.currentSlide)"
       v-bind="imageSettings"
     >
-      <div
-        v-for="image in this.compressedImages"
-        :key="image.id"
-        class="sm:h-80"
-      >
+      <div v-for="index in imageCount" :key="index" class="sm:h-80">
         <img
-          :src="require(`~/static/images/modem-images/${slug}/${image}`)"
+          :src="
+            require(`~/static/images/modem-images/${folderName}/compressed_${
+              index - 1
+            }.jpg`)
+          "
           alt=""
           class="w-full sm:w-auto sm:h-full object-cover mx-auto"
         />
       </div>
     </agile>
-    <agile ref="thumb" v-bind="thumbSettings" class="mt-4 relative">
+    <agile ref="thumbnail" v-bind="thumbnailSettings" class="mt-4 relative">
       <div
-        @click="$refs.main.goTo(index)"
-        v-for="(image, index) in this.thumbnailImages"
+        @click="$refs.main.goTo(index - 1)"
+        v-for="index in imageCount"
         :key="index"
-        class="cursor-pointer border thumb p-2 hover:border-blue-600"
+        class="cursor-pointer border thumbnail p-2 hover:border-blue-600"
       >
         <img
-          :src="require(`~/static/images/modem-images/${slug}/${image}`)"
+          :src="
+            require(`~/static/images/modem-images/${folderName}/150_${
+              index - 1
+            }.jpg`)
+          "
           alt=""
           class="h-12 mx-auto object-contain"
         />
@@ -65,33 +69,10 @@
 </template>
 <script>
 export default {
-  props: ["slug"],
+  props: ["urlPaths", "brand", "model", "imageCount"],
   computed: {
-    imageNames: function () {
-      let imageNames = require
-        .context(`@/static/images/modem-images/comset_cm685vx`, true)
-        .keys();
-
-      imageNames = imageNames.map(function (fileName) {
-        // Ignore the directory './' in string
-        return fileName.slice(2);
-      });
-
-      return imageNames;
-    },
-    compressedImages: function () {
-      let compressedImages = this.imageNames.filter(function (fileName) {
-        let prefix = fileName.split("_")[0];
-        return prefix === "compressed";
-      });
-      return compressedImages;
-    },
-    thumbnailImages: function () {
-      let thumbnailImages = this.imageNames.filter(function (fileName) {
-        let prefix = fileName.split("_")[0];
-        return prefix === "150";
-      });
-      return thumbnailImages;
+    folderName: function () {
+      return `${this.brand}_${this.model}`;
     },
   },
   data() {
@@ -102,7 +83,7 @@ export default {
         navButtons: false,
         infinite: false,
       },
-      thumbSettings: {
+      thumbnailSettings: {
         slidesToShow: 5,
         dots: false,
         infinite: false,
@@ -113,10 +94,47 @@ export default {
 };
 </script>
 <style>
-/* .thumb.agile__slide {
+/* .thumbnail.agile__slide {
   opacity: 80%;
 }
-.thumb.agile__slide--active {
+.thumbnail.agile__slide--active {
   @apply opacity-100;
 } */
 </style>
+// mageNames: function () {
+//       let imageNames = require
+//         .context(`@/static/images/modem-images/${this.folderName}/original`, true)
+//         .keys();
+
+//       imageNames = imageNames.map(function (fileName) {
+//         // Ignore the directory './' in string
+//         return fileName.slice(2);
+//       });
+
+//       return imageNames;
+//     },
+//     compressedImages: function () {
+//       let compressedImages = this.imageNames.filter(function (fileName) {
+//         let prefix = fileName.split("_")[0];
+//         return prefix === "compressed";
+//       });
+//       return compressedImages;
+//     },
+//     thumbnailImages: function () {
+//       let thumbnailImages = this.imageNames.filter(function (fileName) {
+//         let prefix = fileName.split("_")[0];
+//         return prefix === "150";
+//       });
+//       return thumbnailImages;
+//     },
+
+//     imagesCount: function () {
+//       let imagesCount = require
+//         .context(
+//           `@/static/images/modem-images/${this.folderName}/original`,
+//           true
+//         )
+//         .keys();
+
+//       return imagesCount.length;
+//     },
