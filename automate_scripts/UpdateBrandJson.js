2566a101dc26371f1ddf0580e6566b2568fc03e8
huy_trvan <huy_trvan@yahoo.com>
2021-06-13 10:56:45 +0700

    feat: write brands list - static/json/_brand.json

--- automate_scripts/UpdateBrandJson.js
+++ automate_scripts/UpdateBrandJson.js
@@ -1,12 +1,27 @@
 const fs = require('fs');
+const jsonPath = './static/json';
 
-fs.readdir('./static/json/', (error, files) => {
-    brands = files.filter((fileName) => {
-        return !fileName.includes('_');
+// create brands array
+files = fs.readdirSync(jsonPath);
+
+brands = files
+    .filter((file) => {
+        return !file.includes("_");
     });
 
-    brands = brands.map((brand) => {
+brands = brands
+    .map((brand) => {
         return brand.split('.json')[0];
     });
-    console.log(brands);
+
+// create brands Json Array
+brands = brands.map((brand) => {
+    return ["Brand", brand];
 });
+
+brands = JSON.stringify(brands);
+
+// write brands to _brands.json
+const brandJsonPath = jsonPath + '/_brand.json';
+
+fs.writeFileSync(brandJsonPath, brands);
