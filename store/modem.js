6f7bc7a583eaad54f89ffd01e0c1bb6f10db875e
huy_trvan <huy_trvan@yahoo.com>
2021-06-14 11:54:01 +0700

    fix: require _modems.json instead of context

--- store/modem.js
+++ store/modem.js
@@ -8,7 +8,7 @@ export const state = () => ({
 // getters are not functions
 export const getters = {
     fileNames: function () {
-        let fileNames = require.context(`@/static/json/`, false).keys();
+        let fileNames = require('@/static/json/_modems.json');
         return fileNames;
     }
 };
